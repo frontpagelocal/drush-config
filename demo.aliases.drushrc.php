9104a8adc35f99eae3784b9606f266fe1c379c9c
Marc Cepeda <marc@frontpagelocal.com>
2012-08-06 10:10:08 -0700

    added @enrirotite; replaced @demo.dev with @demo.basic

--- demo.aliases.drushrc.php
+++ demo.aliases.drushrc.php
@@ -1,29 +1,8 @@
 <?php
 
 
-$aliases['dev'] = array (
-  'root' => '/home/frontpagelocal/www/dev7',
-  'uri' => 'http://demo.dev7',
-  'path-aliases' => 
-  array (
-    '%drush' => '/usr/share/php/drush',
-    '%site' => 'sites/demo.dev7/',
-  ),
-  /*'databases' => 
-  array (
-    'default' => 
-    array (
-      'default' => 
-      array (
-        'database' => 'dev7_demo',
-        'username' => 'root',
-        'password' => 'fpladmin8756',
-        'host' => 'localhost',
-        'port' => '',
-        'driver' => 'mysql',
-        'prefix' => '',
-      ),
-    ),
-  ),*/
+$aliases['basic'] = array (
+  'root' => '/home/frontpagelocal/basic.frontpagelocal.com',
+  'uri' => 'http://basic.frontpagelocal.com',
 );
 
---
+++ envirotite.aliases.drushrc.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$aliases['stage'] = array (
+  'root' => '/home/frontpagelocal/stage.envirotite.com',
+  'uri' => 'http://stage.envirotite.com',
+);
+
+$aliases['prod'] = array (
+  'root' => '/home/frontpagelocal/www.envirotite.com',
+  'uri' => 'http://www.envirotite.com',
+);
+
