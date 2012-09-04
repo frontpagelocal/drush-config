1a0fddf3b4e4ccfebba29391e6f2cd6dbf9238c2
Marc Cepeda <marc@frontpagelocal.com>
2012-09-04 14:04:18 -0700

    Added sandiegorvrental.com aliases

---
+++ sdrv.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['stage'] = array (
+  'uri' => 'http://stage.sandiegorvrental.com',
+  'root' => '/home/frontpagelocal/stage.sandiegorvrental.com',
+);
+
+$aliases['prod'] = array (
+  'uri' => 'http://www.sandiegorvrental.com',
+  'root' => '/home/frontpagelocal/www.sandiegorvrental.com',
+);
